2fbee6907dc1417a7e6456aeed4ec83f2d0a5387
Saurav Kumar <saurav25400@gmail.com>
2024-06-01 15:32:04 +0530

    Update Homepage.js

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -71,21 +71,15 @@ const Homepage = () => {
                     </p>
                   </li>
                 </ul>
-                // <Link
-                //   to="https://www.udemy.com/course/mern-stack-blogify-project-based-course/?couponCode=08309CBBC4E983226762"
-                //   className="mb-4 mt-9 p-4 inline-block py-3 px-7 w-full leading-6 text-green-50 font-medium text-center bg-gradient-to-r from-green-400 to-blue-600 hover:from-green-500 hover:to-green-700 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md shadow-lg transform transition-all duration-500 ease-in-out hover:scale-105 flex items-center justify-center animate-pulse"
-                // >
-                //   <FaBookOpen className="mr-2" />
-                //   Enroll Now
-                // </Link>
+               
               </div>
-              {/* Register Form */}
+              
               <Register />
             </div>
           </div>
         </div>
       </section>
-      {/* Home posts list */}
+     
       <PublicPosts />
     </div>
   );
